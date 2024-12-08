a0579390ff695f1599823232f604876cd8169471
s-celles <s.celles@gmail.com>
2024-12-08 18:36:39 +0100

    doc

--- docs/make.jl
+++ docs/make.jl
@@ -22,6 +22,4 @@ makedocs(
 # Documenter can also automatically deploy documentation to gh-pages.
 # See "Hosting Documentation" and deploydocs() in the Documenter manual
 # for more information.
-#=deploydocs(
-    repo = "<repository url>"
-)=#
+deploydocs(; repo = "github.com/s-celles/ClassicCiphers.jl")
